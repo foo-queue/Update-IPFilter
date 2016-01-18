b06c77987a3e8446213b08fd317160a3e4e65529
John Belcher <belcherjohn@gmail.com>
2016-01-17 21:01:18 -0500

    Refactored script to make it more easily testable.

--- Update-IPFilter.ps1
+++ Update-IPFilter.ps1
@@ -1,75 +1,94 @@
-﻿[cmdletbinding(PositionalBinding=$false)]
-param(
-    $DestinationDir,
-    $SourceUrl = 'http://upd.emule-security.org/ipfilter.zip',
-    $qBitTorrentPort = 8083,
-    $NumberOfOldFiltersToKeep = 1
-)
-Set-StrictMode -Version 4
+﻿Set-StrictMode -Version 4
 
-if (!$DestinationDir) {
-    $DestinationDir = $PSScriptRoot
-}
+$script:IPFiltersNameFormat = 'ipfilters-{0}.dat'
 
-$log = Join-Path $DestinationDir 'update-ipfilter.log'
-Start-Transcript -LiteralPath $log -Append -Force
-
-$local:ErrorActionPreference = 'Stop'
-try {
+function script:Assert-DestinationDirExists
+{
     if (!(Test-Path -LiteralPath $DestinationDir -PathType Container)) {
         Write-Host "Creating '$DestinationDir'"
         $null = New-Item -Path $DestinationDir -ItemType Directory -Force
     }
+}
 
-    $zipPath = Join-Path $DestinationDir 'ipfilter.zip'
-    $p2pPath = Join-Path $DestinationDir 'guarding.p2p'
-    $ipfiltersPath = Join-Path $DestinationDir "ipfilters-$((Get-Date -Format 'u') -replace '\W','').dat"
-    $ipfiltersPathWildcard = Join-Path $DestinationDir 'ipfilters-*.dat'
+function script:Update-QBitTorrentPrefs {
+    param(
+        $CurrentIPFiltersName
+    )
 
-    Write-Host "Downloading $SourceUrl to '$zipPath'"
-    Invoke-WebRequest -Uri $SourceUrl -OutFile $zipPath -TimeoutSec 30
-
-    Write-Host "Expanding '$zipPath'"
-    Expand-Archive -LiteralPath $zipPath -DestinationPath $DestinationDir -Force
-
-    Write-Host "Renaming '$p2pPath' to '$ipfiltersPath'"
-    Move-Item -LiteralPath $p2pPath -Destination $ipfiltersPath -Force
-
-    # Refresh qBitTorrent...
-    if (Get-Process -Name 'qbittorrent' -ErrorAction Ignore) {
-        Write-Host 'Updating qBitTorrent preferences'
-        $preferences = @{
-            'ip_filter_enabled' = $true
-            'ip_filter_path' = "$ipfiltersPath"
-        }
-        $qBitTorrentWebClientUrl = "http://localhost:$qBitTorrentPort/command/setPreferences"
-
-        Add-Type -AssemblyName 'System.Web.Extensions'
-        $jsonSerializer = New-Object 'System.Web.Script.Serialization.JavaScriptSerializer'
-        $json = $jsonSerializer.Serialize($preferences.PSObject.BaseObject)
-
-        Write-Host "Posting $json to $qBitTorrentWebClientUrl"
-        $null = Invoke-WebRequest `
-            -Uri $qBitTorrentWebClientUrl `
-            -Body "json=$json" `
-            -Method Post
+    $url = "$qBitTorrentWebClientUrl/command/setPreferences"
+    $preferences = @{
+        'ip_filter_enabled' = $true
+        'ip_filter_path' = "$(Join-Path $DestinationDir $CurrentIPFiltersName)"
     }
-    else {
-        Write-Host 'qBitTorrent is not running.'
-    }
+    
+    Add-Type -AssemblyName 'System.Web.Extensions'
+    $jsonSerializer = New-Object 'System.Web.Script.Serialization.JavaScriptSerializer'
+    $json = $jsonSerializer.Serialize($preferences)
 
-    # Delete old ipfilters...
-    Get-ChildItem -Path $ipfiltersPathWildcard -Exclude (Split-Path $ipfiltersPath -Leaf) | 
+    Write-Verbose "Posting $json to $url"
+    $null = Invoke-WebRequest -Uri $url -Body "json=$json" -Method Post
+}
+
+function script:Remove-OldIPFilters {
+    param(
+        $CurrentIPFiltersName
+    )
+
+    Get-ChildItem -Path (Join-Path $DestinationDir ($IPFiltersNameFormat -f '*')) -Exclude $CurrentIPFiltersName | 
         Sort-Object Name -Descending | 
         Select-Object -Skip $NumberOfOldFiltersToKeep | 
         ForEach-Object { Write-Host "Deleting old ipfilter: $_"; $_} | 
         Remove-Item -ea Continue
 }
-catch {
-    Remove-Item variable:\ErrorActionPreference -ea Ignore
-    $_ | Write-Error
-    return
-}
-finally {
-    Stop-Transcript
+
+function Update-IPFilter {
+
+    [cmdletbinding(PositionalBinding=$false)]
+    param(
+        [string] $DestinationDir,
+        [string] $SourceUrl = 'http://upd.emule-security.org/ipfilter.zip',
+        [AllowEmptyString()]
+        [string] $qBitTorrentWebClientUrl = 'http://localhost:8083',
+        [int] $NumberOfOldFiltersToKeep = 1
+    )
+
+    if (!$DestinationDir) {
+        $DestinationDir = $PSScriptRoot
+    }
+
+    $log = Join-Path $DestinationDir 'Update-IPFilter.log'
+    Start-Transcript -LiteralPath $log -Append -Force
+
+    $local:ErrorActionPreference = 'Stop'
+    try {
+        Assert-DestinationDirExists
+
+        $ipfiltersName = $IPFiltersNameFormat -f ((Get-Date).ToString('u') -replace '\W','')
+        $zipPath = Join-Path $DestinationDir 'ipfilter.zip'
+        $p2pPath = Join-Path $DestinationDir 'guarding.p2p'
+
+        Write-Host "Downloading $SourceUrl to '$zipPath'"
+        Invoke-WebRequest -Uri $SourceUrl -OutFile $zipPath -TimeoutSec 30
+
+        Write-Host "Expanding '$zipPath'"
+        Expand-Archive -LiteralPath $zipPath -DestinationPath $DestinationDir -Force
+
+        Write-Host "Renaming '$p2pPath' to '$ipfiltersName'"
+        Rename-Item -LiteralPath $p2pPath -NewName $ipfiltersName -Force
+
+        if ($qBitTorrentWebClientUrl) {
+            Write-Host 'Updating qBitTorrent'
+            Update-QBitTorrentPrefs $ipfiltersName
+        }
+
+        Remove-OldIPFilters $ipfiltersName
+    }
+    catch {
+        Remove-Variable -Name 'ErrorActionPreference' -ea Ignore
+        $_ | Write-Error
+        return
+    }
+    finally {
+        Stop-Transcript
+    }
 }
